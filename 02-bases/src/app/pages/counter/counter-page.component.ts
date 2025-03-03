f95459363abfba2663698451334599d8d292e13e
Diego Santacruz <dsantacruz@gmail.com>
2025-03-03 18:51:58 -0300

    signal

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
@@ -7,11 +7,16 @@ import { Component } from '@angular/core';
 export class CounterPageComponent {
   counter: number = 10;
 
+  counterSignal = signal(10);
+
   increaseByOne(value: number) {
     this.counter += value;
+    //this.counterSignal.set(this.counterSignal()+value);
+    this.counterSignal.update((current) => current + value);
   }
 
   resetCounter() {
-    this.counter = 10;
+    this.counter = 0;
+    this.counterSignal.set(0);
   }
 }
